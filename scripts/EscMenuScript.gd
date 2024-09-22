b4d78d86d8f736ce39dfc0065dfea31df8410829
Parker Vetter <vette107@d.umn.edu>
2024-09-22 17:41:06 -0500

    added the loading scene system to the game so people can go back and choose different levels

--- scripts/EscMenuScript.gd
+++ scripts/EscMenuScript.gd
@@ -11,6 +11,11 @@ func _on_settings_button_pressed() -> void:
 	pass # Replace with function body.
 
 
+func _on_return_to_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Levels/scene_switcher.tscn")
+
+
 func _on_close_game_button_pressed() -> void:
 	# This will close the game
 	get_tree().quit()
+	
--- scripts/sceneSwitcher.gd
+++ scripts/sceneSwitcher.gd
@@ -1,12 +1,14 @@
 extends Node
 
-@onready var currentScene = %MainMenu
+@onready var MainMenuScene = %MainMenu
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	MainMenuScene.connect("world_one_load", Callable(self, "world_one_load_button_pressed"))
+	MainMenuScene.connect("world_two_load", Callable(self, "world_two_load_button_pressed"))
 
+func world_one_load_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/Levels/level1.tscn")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func world_two_load_button_pressed():
+	print("need to make world 2")
